826e05ecd8c5e2c969b2f9a6a8a4833c139b15f0
friendnj <810455341@qq.com>
2017-09-21 23:41:13 +0800

    9.21

--- www/orm.py
+++ www/orm.py
@@ -1,5 +1,16 @@
+import asyncio,logging
+import aiomysql
+
+
+
+
+
+
+
+def log(sql,args=()):
+	logging.info('SQL:%s'%sql)
 @asyncio.coroutine
-def create_pool(loop,**kw):
+async def create_pool(loop,**kw):
 	logging.info('create database connection pool...')
 	global __pool
 	__pool = yield from aiomysql.create_pool(
@@ -14,32 +25,130 @@ def create_pool(loop,**kw):
 		minsize=kw.get('minsize',1),
 		loop=loop
 	)
-
 @asyncio.coroutine
-def select(sql,args,size=None):
+async def select(sql,args,size=None):
 	log(sql,args)
 	global __pool
-	with (yield from __pool) as conn:
-		cur = yield from conn.cursor(aiomysql.DictCursor)
-		yield from cur.execute(sql.replace('?','%s'),args or ())
-		if size:
-			rs = yield from cur.fetchmany(size)
-		else:
-			rs = yield from cur.fetchall()
-		yield from cur.close()
+	async with __pool.get() as conn:
+		async with conn.cursor(aiomysql.DictCursor) as cur:
+			await cur.execute(sql.replace('?','%s'),args or ())
+			if size:
+				rs = await cur.fetchmany(size)
+			else:
+				rs = await cur.fetchall()
 		logging.info('rows returned:%s' % len(rs))
 		return rs
-
 @asyncio.coroutine
-def execute(sql,args):
+async def execute(sql,args,autocommit=True):
 	log(sql)
-	with (yield from __pool) as conn:
+	async with __pool.get() as conn:
+	    if not autocommit:
+	    	await conn.begin()
 		try:
-			cur= yield from conn.cursor()
-			yield from cur.execute(sql.replace('?','%s'),args)
-			affected = cur.rowcount
-			yield from cur.close()
+			async with conn.cursor(aiomysql.DictCursor) as cur:
+				await cur.execute(sql.replace('?','%s'),args)
+				affected = cur.rowcount
+			if not autocommit:
+				await conn.commit()
 		except BaseException as e:
+			if not autocommit:
+				await conn.rollback()
 			raise
 		return affected
+
+def create_args_string(num):
+	L=[]
+	for n in range(num):
+		L.append('?')
+	return ','.join(L)
+
+class Field(object):
+
+	def __init__(self,name,column_type,primary_key,default):
+		self.name = name
+		self.column_type = column_type
+		self.primary_key = primary_key
+		self.default = default
+
+	def __str__(self):
+		return '<%s.%s:%s>'%(self.__class__.__name__,self.column_type,self.name)
+
+class StringField(Field):
+
+	def __init__(self,name=None,primary_key=False,default=None,ddl='varchar(100)'):
+		super().__init__(name,ddl,primary_key,default)
+
+class BooleanField(Field):
+
+	def __init__(self,name=None,default=False):
+		super().__init__(name,'boolean',False,default)
+
+class IntegerField(Field):
+
+	def __init__(self,name=None,primary_key=False,default=0):
+		super().__init__(name,'bigint',primary_key,default)
+
+class FloatField(Field):
+
+	def __init__(self,name=None,primary_key=False,default=0):
+		super().__init__(name,'real',primary_key,default)
+
+class TextField(Field):
+
+	def __init__(self,name=None,default=None):
+		super.__init__(name,'text',False,default)
+
+class ModelMetaclass(type):
+
+	def __new__(cls,name,bases,attrs):
+		if name=='Model':
+			return type.__new__(cls,name,bases,attrs)
+		tableName=attrs.get('__table__',None) or name
+		logging.info('found model:%s(table:%s)'%(name,tableName))
+		mappings=dict()
+		fields=[]
+		primary_key =None
+		for k,v in attrs.items():
+			if isinstance(v,Field):
+				logging.info('found mapping:%s==>%s'%(k,v))
+				mappings[k]=v
+				if v.primary_key:
+					#
+					if primaryKey:
+						raise StandardError('Duplicate primary ket for field:%s'%k)
+					primaryKey = k
+			    else:
+			    	fields.append(k)
+	    if not primaryKey:
+	    	raise StandardError('Primary key not found.')
+	    for k in mappings.keys():
+	    	attrs.pop(k)
+	    escaped_fields = list(map(lambda f:'`%s`'% f,fields))
+	    attrs['__mappings__']= mappings
+	    attrs['__table__']=tableName
+	    attrs['__primary_key__']=primaryKey
+	    attrs['__fields__']=fields
+	    attrs['__select__']='select`%s`,%s from `%s`'%(primaryKey,','.join(escaped_fields),tableName)
+	    attrs['__insert__']='insert into `%s`(%s,`%s`) values(%s)'%(tableName,','.join(escaped_fields),primaryKey,create_args_string(len(escaped_fields)+1))
+	    attrs['__update__']='update `%s` set %s where `%s`=?'%(tableName,','.join(map(lambda f:'`%s`=?'%(mapping.get(f).name or f),fields)),primaryKey)
+	    attrs['__delete__']='delete from `%s` where `%s`=?'%(tableName.primaryKey)
+	    return type.__new__(cls,name,bases,attrs)
+
+class Model(dict,metaclass=ModelMetaclass):
+
+	def __init__(self,**kw):
+		super(Model,self).__init__(**kw)
+
+	def __getattr__(self,key):
+		try:
+			return self[key]:
+		except KeyError:
+			raise AttributeError(r"'Model' object has no attribute '%s'"%key)
+
+	
+		
+		
+		
+		
+
 
